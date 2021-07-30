e53cded2fb3836451d5c8548410adaa816a6ebf7
Mar Antonio Bacalla <system_developer4u@yahoo.com>
2021-07-30 16:56:49 +0800

    Attendance monitoring

--- attendanceloadprev.php
+++ attendanceloadprev.php
@@ -13,29 +13,9 @@ $prevdate = $_POST['prevdate'];
 
 	<?php
 
-	if($attbranch == '' && $attdept == '')
-	{
-		$query = "SELECT wk.name as name, pos.name as position, dm.name as department,bra.name as branch, 
-					MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timein',
-					MAX(case when mt.type = 1 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timeout' 
+	$query = "call get_attendance('$dataareaid', '$prevdate', '$attbranch', '$attdept');"
 
-					from monitoringtable mt 
-					LEFT JOIN worker wk ON mt.Name = wk.BioId left join branch brn on wk.branch = brn.branchcode and wk.dataareaid = brn.dataareaid
-					left join position pos on pos.positionid = wk.position and pos.dataareaid = wk.dataareaid 
-					LEFT JOIN contract con ON wk.workerid = con.workerid and con.dataareaid = wk.dataareaid 
-					LEFT JOIN department dm ON con.departmentid = dm.departmentid and wk.dataareaid = dm.dataareaid 
-					left join branch bra on bra.branchcode = wk.branch and bra.dataareaid = wk.dataareaid
-					where 
-
-					mt.Date = '$prevdate' and 
-					wk.dataareaid = '$dataareaid' 
-					#and dm.departmentid = '$attdept'
-
-					group by wk.name  
-
-
-					order by MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) asc";
-		$result = $conn->query($query);
+	$result = $conn->query($query);
 		$rowclass = "rowA";
 		$rowcnt = 0;
 		$rowcnt2 = 0;
@@ -44,175 +24,31 @@ $prevdate = $_POST['prevdate'];
 		{ 
 			$rowcnt++;
 			$rowcnt2++;
-				if($rowcnt > 1) { $rowcnt = 0; $rowclass = "rowB"; }
-				else { $rowclass = "rowA";}
+			if($rowcnt > 1)
+			{ 
+				$rowcnt = 0;
+				$rowclass = "rowB";
+			}
+			else
+			{
+				$rowclass = "rowA";
+			}
 				
-			?>
-			<tr  class="<?php echo $rowclass; ?>" tabindex="<?php echo $rowcnt2; ?>">
-				<!--<td style="width:10px;"><input type='checkbox' name="chkbox" value="" id="myCheck"></td>-->
-				<td style="width:20px;" class="text-center"><span class="fa fa-angle-right"></span></td>
-				<td style="width:20%;"><?php echo $row['name'];?></td>
-				<td style="width:20%;"><?php echo $row['position'];?></td>
-				<td style="width:20%;"><?php echo $row['department'];?></td>
-				<td style="width:20%;"><?php echo $row['branch'];?></td>
-				<td style="width:20%;"><?php echo $row['timein'];?></td>
-				<td style="width:20%;"><?php echo $row['timeout'];?></td>
-				
-			</tr>
+		?>	
 
-		<?php }
-	}
-	else if($attbranch != '' && $attdept == '')
-	{
-		$query = "SELECT wk.name as name, pos.name as position, dm.name as department,bra.name as branch, 
-					MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timein',
-					MAX(case when mt.type = 1 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timeout' 
+		<tr  class="<?php echo $rowclass; ?>" tabindex="<?php echo $rowcnt2; ?>">
+			<!--<td style="width:10px;"><input type='checkbox' name="chkbox" value="" id="myCheck"></td>-->
+			<td style="width:20px;" class="text-center"><span class="fa fa-angle-right"></span></td>
+			<td style="width:20%;"><?php echo $row['name'];?></td>
+			<td style="width:20%;"><?php echo $row['position'];?></td>
+			<td style="width:20%;"><?php echo $row['department'];?></td>
+			<td style="width:20%;"><?php echo $row['branch'];?></td>
+			<td style="width:20%;"><?php echo $row['timein'];?></td>
+			<td style="width:20%;"><?php echo $row['timeout'];?></td>
+		</tr>
 
-					from monitoringtable mt 
-					LEFT JOIN worker wk ON mt.Name = wk.BioId left join branch brn on wk.branch = brn.branchcode and wk.dataareaid = brn.dataareaid 
-					left join position pos on pos.positionid = wk.position and pos.dataareaid = wk.dataareaid
-					LEFT JOIN contract con ON wk.workerid = con.workerid and con.dataareaid = wk.dataareaid 
-					LEFT JOIN department dm ON con.departmentid = dm.departmentid and wk.dataareaid = dm.dataareaid 
-					left join branch bra on bra.branchcode = wk.branch and bra.dataareaid = wk.dataareaid
-					where 
-
-					mt.Date = '$prevdate' and 
-					wk.dataareaid = '$dataareaid' 
-					and bra.branchcode = '$attbranch'
-					
-
-					group by wk.name  
-
-
-					order by MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) asc";
-		$result = $conn->query($query);
-		$rowclass = "rowA";
-		$rowcnt = 0;
-		$rowcnt2 = 0;
-		
-		while ($row = $result->fetch_assoc())
-		{ 
-			$rowcnt++;
-			$rowcnt2++;
-				if($rowcnt > 1) { $rowcnt = 0; $rowclass = "rowB"; }
-				else { $rowclass = "rowA";}
-				
-			?>
-			<tr  class="<?php echo $rowclass; ?>" tabindex="<?php echo $rowcnt2; ?>">
-				<!--<td style="width:10px;"><input type='checkbox' name="chkbox" value="" id="myCheck"></td>-->
-				<td style="width:20px;" class="text-center"><span class="fa fa-angle-right"></span></td>
-				<td style="width:20%;"><?php echo $row['name'];?></td>
-				<td style="width:20%;"><?php echo $row['position'];?></td>
-				<td style="width:20%;"><?php echo $row['department'];?></td>
-				<td style="width:20%;"><?php echo $row['branch'];?></td>
-				<td style="width:20%;"><?php echo $row['timein'];?></td>
-				<td style="width:20%;"><?php echo $row['timeout'];?></td>
-				
-			</tr>
-
-		<?php }
-	}
-	else if($attbranch == '' && $attdept != '')
-	{
-		$query = "SELECT wk.name as name, pos.name as position, dm.name as department,bra.name as branch, 
-					MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timein',
-					MAX(case when mt.type = 1 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timeout' 
-
-					from monitoringtable mt 
-					LEFT JOIN worker wk ON mt.Name = wk.BioId left join branch brn on wk.branch = brn.branchcode and wk.dataareaid = brn.dataareaid 
-					left join position pos on pos.positionid = wk.position and pos.dataareaid = wk.dataareaid
-					LEFT JOIN contract con ON wk.workerid = con.workerid and con.dataareaid = wk.dataareaid 
-					LEFT JOIN department dm ON con.departmentid = dm.departmentid and wk.dataareaid = dm.dataareaid 
-					left join branch bra on bra.branchcode = wk.branch and bra.dataareaid = wk.dataareaid
-					where 
-
-					mt.Date = '$prevdate' and 
-					wk.dataareaid = '$dataareaid' 
-					and dm.departmentid = '$attdept'
-
-					group by wk.name  
-
-
-					order by MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) asc";
-		$result = $conn->query($query);
-		$rowclass = "rowA";
-		$rowcnt = 0;
-		$rowcnt2 = 0;
-		
-		while ($row = $result->fetch_assoc())
-		{ 
-			$rowcnt++;
-			$rowcnt2++;
-				if($rowcnt > 1) { $rowcnt = 0; $rowclass = "rowB"; }
-				else { $rowclass = "rowA";}
-				
-			?>
-			<tr  class="<?php echo $rowclass; ?>" tabindex="<?php echo $rowcnt2; ?>">
-				<!--<td style="width:10px;"><input type='checkbox' name="chkbox" value="" id="myCheck"></td>-->
-				<td style="width:20px;" class="text-center"><span class="fa fa-angle-right"></span></td>
-				<td style="width:20%;"><?php echo $row['name'];?></td>
-				<td style="width:20%;"><?php echo $row['position'];?></td>
-				<td style="width:20%;"><?php echo $row['department'];?></td>
-				<td style="width:20%;"><?php echo $row['branch'];?></td>
-				<td style="width:20%;"><?php echo $row['timein'];?></td>
-				<td style="width:20%;"><?php echo $row['timeout'];?></td>
-				
-			</tr>
-
-		<?php }
-	}
-	else if ($attbranch != '' && $attdept != '') {
-		$query = "SELECT wk.name as name, pos.name as position, dm.name as department,bra.name as branch, 
-					MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timein',
-					MAX(case when mt.type = 1 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) as 'timeout' 
-
-					from monitoringtable mt 
-					LEFT JOIN worker wk ON mt.Name = wk.BioId left join branch brn on wk.branch = brn.branchcode and wk.dataareaid = brn.dataareaid 
-					left join position pos on pos.positionid = wk.position and pos.dataareaid = wk.dataareaid
-					LEFT JOIN contract con ON wk.workerid = con.workerid and con.dataareaid = wk.dataareaid 
-					LEFT JOIN department dm ON con.departmentid = dm.departmentid and wk.dataareaid = dm.dataareaid 
-					left join branch bra on bra.branchcode = wk.branch and bra.dataareaid = wk.dataareaid
-					where 
-
-					mt.Date = '$prevdate' and 
-					wk.dataareaid = '$dataareaid' 
-					and bra.branchcode = '$attbranch'
-					and dm.departmentid = '$attdept'
-
-					group by wk.name  
-
-
-					order by MIN(case when mt.type = 0 then TIME_FORMAT(mt.Time,'%h:%i %p') else null end) asc";
-		$result = $conn->query($query);
-		$rowclass = "rowA";
-		$rowcnt = 0;
-		$rowcnt2 = 0;
-		
-		while ($row = $result->fetch_assoc())
-		{ 
-			$rowcnt++;
-			$rowcnt2++;
-				if($rowcnt > 1) { $rowcnt = 0; $rowclass = "rowB"; }
-				else { $rowclass = "rowA";}
-				
-			?>
-			<tr  class="<?php echo $rowclass; ?>" tabindex="<?php echo $rowcnt2; ?>">
-				<!--<td style="width:10px;"><input type='checkbox' name="chkbox" value="" id="myCheck"></td>-->
-				<td style="width:20px;" class="text-center"><span class="fa fa-angle-right"></span></td>
-				<td style="width:20%;"><?php echo $row['name'];?></td>
-				<td style="width:20%;"><?php echo $row['position'];?></td>
-				<td style="width:20%;"><?php echo $row['department'];?></td>
-				<td style="width:20%;"><?php echo $row['branch'];?></td>
-				<td style="width:20%;"><?php echo $row['timein'];?></td>
-				<td style="width:20%;"><?php echo $row['timeout'];?></td>
-				
-			</tr>
-
-		<?php }
-	
-	}
-		
-
+	<?php 
+		}
 	?>
 
 
